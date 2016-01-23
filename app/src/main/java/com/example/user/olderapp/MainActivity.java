50286c7628887b94f9d36aa216d42d53e5d57c76
jeremywahaha <jeremy08300830@gmail.com>
2016-01-23 15:19:44 +0800

    UI

--- app/src/main/java/com/example/user/olderapp/MainActivity.java
+++ app/src/main/java/com/example/user/olderapp/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.user.olderapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button B1,B2,B3,B4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       B1 = (Button)this.findViewById(R.id.button1);
+        B2 = (Button)this.findViewById(R.id.button2);
+        B3 = (Button)this.findViewById(R.id.button3);
+        B4 = (Button)this.findViewById(R.id.button4);
+
+        B1.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v){
+                Intent EatDrug = new Intent();
+                EatDrug.setClass(MainActivity.this.);
+                startActivity(EatDrug);
+            }
+        });
+
+        B2.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v){
+                Intent PhoneCall = new Intent();
+                PhoneCall.setClass(MainActivity.this.);
+                startActivity(PhoneCall);
+            }
+        });
+
+        B3.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v){
+                Intent IsLost = new Intent();
+                IsLost.setClass(MainActivity.this. );
+                startActivity(IsLost);
+            }
+        });
+
+        B4.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v){
+                Intent SelfInfo = new Intent();
+                SelfInfo.setClass(MainActivity.this. );
+                startActivity(SelfInfo);
+            }
+        });
     }
 }
