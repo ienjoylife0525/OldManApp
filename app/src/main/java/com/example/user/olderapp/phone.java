a836cd8c24ea4a51519da49b233add998e199725
pp724181 <pp724181@yahoo.com.tw>
2016-01-23 16:28:38 +0800

    call 119 complete

--- app/src/main/java/com/example/user/olderapp/phone.java
+++ app/src/main/java/com/example/user/olderapp/phone.java
@@ -18,11 +18,23 @@ public abstract class phone extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.phonecall);
         Button b = (Button)this.findViewById(R.id.policecall);
         b.setOnClickListener(this);
+        Button b1 = (Button)this.findViewById(R.id.family);
+        b1.setOnClickListener(this);
+        Button b2 = (Button)this.findViewById(R.id.help);
+        b1.setOnClickListener(this);
     }
 
     public void call_police(View view) {
         Intent phoneIntent = new Intent(Intent.ACTION_CALL);
         phoneIntent.setData(Uri.parse("tel:110"));
+    }
+    public void callfamily(View view) {
+        Intent phoneIntent = new Intent(Intent.ACTION_CALL);
+        phoneIntent.setData(Uri.parse("tel:123456789"));
+    }
+    public void helpme(View view) {
+        Intent phoneIntent = new Intent(Intent.ACTION_CALL);
+        phoneIntent.setData(Uri.parse("tel:119"));
     }
 
 }
